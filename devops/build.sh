9f5bcbea7b29c5e8c2063da15ce0faf1e04f774d
Justin Merz <jrmerz@gmail.com>
2023-09-14 11:43:10 -0700

    fix for multi arch builds

--- devops/build.sh
+++ devops/build.sh
@@ -9,10 +9,10 @@ source config.sh
 
 #! /bin/bash
 
-DOCKER_BUILD="docker build"
-
 # for google cloud multi-arch builds
 if [[ $LOCAL_DEV == 'true' ]]; then
+  DOCKER_BUILD="docker build"
+else
   docker context create amd_node --docker "host=unix:///var/run/docker.sock"
   docker context create arm_node --docker "host=ssh://ci-bot@$ARM64_MACHINE_IP"
 
