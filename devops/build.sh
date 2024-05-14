011ceb78e6e0c5b61558b65bb28a12eda53a3f62
Justin Merz <jrmerz@gmail.com>
2024-05-14 12:27:14 -0700

    updates

--- devops/build.sh
+++ devops/build.sh
@@ -13,7 +13,8 @@ DOCKER_BUILD="docker buildx build"
 
 # for google cloud multi-arch builds
 if [[ $LOCAL_DEV == 'true' ]]; then
-  DOCKER_BUILD="$DOCKER_BUILD --pull --output=type=docker"
+  # DOCKER_BUILD="$DOCKER_BUILD --pull --output=type=docker"
+  DOCKER_BUILD="$DOCKER_BUILD --pull"
 else
   docker context create amd_node --docker "host=unix:///var/run/docker.sock"
   docker context create arm_node --docker "host=ssh://ci-bot@$ARM64_MACHINE_IP"
@@ -27,10 +28,23 @@ fi
 
 
 echo "building: $NODE_IMAGE_NAME:$BRANCH_NAME"
-$DOCKER_BUILD \
-  --cache-to=type=inline \
+
+BASE_BUILD="$DOCKER_BUILD \
   --cache-from $NODE_IMAGE_NAME:$BRANCH_NAME \
   -t $NODE_IMAGE_NAME:$BRANCH_NAME \
   --build-arg NODE_VERSION=$NODE_VERSION \
-  -f ../Dockerfile \
-  ../
+  -f ../Dockerfile"
+
+$BASE_BUILD ../
+
+if [[ $LOCAL_DEV != 'true' ]]; then
+  echo "stopping buildx builder"
+  docker buildx stop ucd-lib-builder || true
+
+  echo "building with cache and pushing"
+  $BASE_BUILD \
+    --cache-from $NODE_IMAGE_NAME:$BRANCH_NAME \
+    --cache-to=type=inline \
+    --push \
+    ../
+fi
